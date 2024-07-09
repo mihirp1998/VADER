82f8c3a76a31cbac6dc12aca93ff55829ae20949
QinOwen <110217995+QinOwen@users.noreply.github.com>
2024-07-09 02:46:52 -0400

    add-download-lora-pretrained-model-update

--- VADER-VideoCrafter/scripts/run_text2video_inference_temp.sh
+++
@@ -1,6 +0,0 @@
-name="base_512_v2"
-ckpt='checkpoints/base_512_v2/model.ckpt'
-config='configs/inference_t2v_512_v2.0.yaml'
-prompt_file="prompts/test_prompts.txt"
-res_dir="results"
-accelerate launch --multi_gpu --main_process_port 25001 scripts/main/train_t2v_lora.py --seed 200 --mode 'base' --ckpt_path 'checkpoints/base_512_v2/model.ckpt' --config 'configs/inference_t2v_512_v2.0.yaml' --height 320 --width 512 --unconditional_guidance_scale 12.0 --ddim_steps 25 --ddim_eta 1.0 --frames 24 --prompt_fn 'chatgpt_custom_cute' --val_batch_size 1 --num_val_runs 1 --lora_rank 16 --inference_only True --project_dir ./project_dir/inference --lora_ckpt_path 'huggingface-pickscore' --is_sample_preview True
